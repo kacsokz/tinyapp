737faf96c85a854583194b5cde18ecf1294f8157
kacsokz <casey.sokach@gmail.com>
2019-08-05 21:07:36 +0000

    Added show page, second route and template

--- express_server.js
+++ express_server.js
@@ -19,6 +19,11 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase.shortURL };
+  res.render("urls_show", templateVars);
+});
+
 app.get('/', (req, res) => {
   res.send('Hello!');
 });
