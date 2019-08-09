cb460e2b967b27ec9427a3ca89b21fe9700198dd
kacsokz <casey.sokach@gmail.com>
2019-08-09 21:19:42 +0000

    Added Mocha & Chai as dev-dependencies and created testing for helper function

--- helpers.js
+++ helpers.js
@@ -1,4 +1,3 @@
-
 const getUserByEmail = function(email, database) {
   for (let users in database) {
     const user = database[users];
@@ -7,7 +6,7 @@ const getUserByEmail = function(email, database) {
       return user;
     }
   }
-  return null;
+  return undefined;
 };
 
 module.exports = { getUserByEmail };
